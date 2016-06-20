c8fce51293191e6703ce183201371fa19ef80ec5
kanakoiso <g1527008@st.t-kougei.ac.jp>
2016-06-20 17:29:31 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -4,6 +4,8 @@ document.getElementById('choice').textContent = new Date();
 var number = Math.floor(Math.random()*3);
 var answer = parseInt(window.prompt('数あてゲーム。0～2の数字を入力してたください!'));
 
+
+
 var message;
 
 if(answer === number){
