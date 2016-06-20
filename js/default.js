13b27a026590986354c4c023dd1fd072c98a7aed
kanakoiso <g1527008@st.t-kougei.ac.jp>
2016-06-20 17:50:37 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,6 +1,6 @@
 
 var number = Math.floor(Math.random()*3);
-var answer = parseInt(window.prompt('数あてゲーム。0～2の数字を入力してたください!'));
+var answer = parseInt(window.prompt('数あてゲーム。0～2の数字を入力してください!'));
 
 
 
